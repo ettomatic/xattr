1a007d894defd179992ba1966917c7c487ebc00a
ettomatic <ettore.berardi@outlook.com>
2020-06-15 22:31:50 +0100

    Move Lib inside Xattr namespace and use Lib::Char instead of Uint8.

--- src/xattr.cr
+++ src/xattr.cr
@@ -1,49 +1,49 @@
-lib C
-  fun getxattr(path : UInt8*, name : UInt8*, pointer : UInt8*, size : LibC::SizeT) : LibC::Int
-  fun setxattr(path : UInt8*, name : UInt8*, value : UInt8*, pointer : UInt32, size : LibC::SizeT) : LibC::Int
-  fun listxattr(path : UInt8*, pointer : UInt8*, size : LibC::SizeT) : LibC::Int
-  fun removexattr(path : UInt8*, name : UInt8*) : LibC::Int
-end
-
 # Crystal bindings to XATTR.
 # This library allow to manage extended file attributes (XATTR) as file metadata.
 class XAttr
   VERSION = "0.4.0"
+
+  lib LibXAttr
+    fun getxattr(path : LibC::Char*, name : LibC::Char*, pointer : LibC::Char*, size : LibC::SizeT) : LibC::Int
+    fun setxattr(path : LibC::Char*, name : LibC::Char*, value : LibC::Char*, pointer : UInt32, size : LibC::SizeT) : LibC::Int
+    fun listxattr(path : LibC::Char*, pointer : LibC::Char*, size : LibC::SizeT) : LibC::Int
+    fun removexattr(path : LibC::Char*, name : LibC::Char*) : LibC::Int
+  end
 
   def initialize(path : String)
     @path = path
   end
 
   def [](key)
-    size = C.getxattr(@path, key, nil, 0)
+    size = LibXAttr.getxattr(@path, key, nil, 0)
     return unless size > 0
 
-    ptr = Slice(UInt8).new(size)
-    res = C.getxattr(@path, key, ptr, size)
+    ptr = Slice(LibC::Char).new(size)
+    res = LibXAttr.getxattr(@path, key, ptr, size)
     raise_error if res == -1
 
     String.new(ptr)
   end
 
   def []=(key, value)
-    res = C.setxattr(@path, key, value, value.bytesize, 0)
+    res = LibXAttr.setxattr(@path, key, value, value.bytesize, 0)
     raise_error if res == -1
 
     res
   end
 
   def list
-    size = C.listxattr(@path, nil, 0)
+    size = LibXAttr.listxattr(@path, nil, 0)
     return [] of String unless size > 0
 
-    ptr = Slice(UInt8).new(size)
-    C.listxattr(@path, ptr, size)
+    ptr = Slice(LibC::Char).new(size)
+    LibXAttr.listxattr(@path, ptr, size)
 
     String.new(ptr).split("\000", remove_empty: true)
   end
 
   def remove(key)
-    res = C.removexattr(@path, key)
+    res = LibXAttr.removexattr(@path, key)
     raise_error if res == -1
 
     res
