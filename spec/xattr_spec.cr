7b00a8205e82d288ff1461fee0e99340f257d997
ettomatic <ettore.berardi@outlook.com>
2020-06-15 14:11:37 +0100

    Adds Xattr.list method.

--- spec/xattr_spec.cr
+++ spec/xattr_spec.cr
@@ -4,7 +4,7 @@ describe XAttr do
   key = "user.xdg.tags"
 
   describe ".get" do
-    it "returns the tags assigned to a target file" do
+    it "returns the specific xattr value assigned to a target file" do
       path = "spec/test_get.txt"
       file = File.touch(path)
 
@@ -37,9 +37,35 @@ describe XAttr do
       File.delete(path)
     end
 
-    it "raise an erro if the target file is missing" do
+    it "raise an exception if the target file is missing" do
       expect_raises(IO::Error, "ENOENT - please check the target file") do
         XAttr.set("spec/not_there.txt", key, "mytag1")
+      end
+    end
+  end
+
+  describe ".list" do
+    context "with xattrs set on the target file" do
+      it "returns the attrs assigned to a target file" do
+        path = "spec/test_list.txt"
+        file = File.touch(path)
+        XAttr.set(path, key, "mytag1")
+        XAttr.set(path, "user.xdg.comments", "foobar")
+
+        XAttr.list(path).should eq ["user.xdg.tags", "user.xdg.comments"]
+
+        File.delete(path)
+      end
+    end
+
+    context "with no xattrs set on the file" do
+      it "returns the attrs assigned to a target file" do
+        path = "spec/test_list.txt"
+        file = File.touch(path)
+
+        XAttr.list(path).should eq [] of String
+
+        File.delete(path)
       end
     end
   end
--- src/xattr.cr
+++ src/xattr.cr
@@ -1,6 +1,7 @@
 lib C
   fun getxattr(path: UInt8*, name: UInt8*, pointer: UInt8*, size: LibC::SizeT) : LibC::Int
   fun setxattr(path: UInt8*, name: UInt8*, value: UInt8*, pointer: UInt32, size: LibC::SizeT) : LibC::Int
+  fun listxattr(path: UInt8*, pointer: UInt8*, size: LibC::SizeT) : LibC::Int
 end
 
 module XAttr
@@ -22,6 +23,15 @@ module XAttr
     raise_error if res == -1
 
     res
+  end
+
+  def self.list(path)
+    size = C.listxattr(path, nil, 0)
+
+    ptr = Slice(UInt8).new(size)
+    C.listxattr(path, ptr, size)
+
+    String.new(ptr).split("\000", remove_empty: true)
   end
 
   def self.raise_error
