c3e4503581ef35e87c706f2942aca19006a506db
iain barnett <helpful-iain@theprintedbird.com>
2021-07-07 02:00:44 +0900

    Should handle paths too.

--- src/xattr/xattr.cr
+++ src/xattr/xattr.cr
@@ -1,44 +1,41 @@
 module XAttr
   class XAttr
-    def initialize(path : String, no_follow = false, only_create = false, only_replace = false)
-      @path = path
-      @no_follow = no_follow
-      @only_create = only_create
-      @only_replace = only_replace
+    def initialize(@path : String | Path, @no_follow = false, @only_create = false, @only_replace = false)
+			@path = Path.new(@path).expand
     end
 
     def [](key)
-      size = bindings.get(@path, key, nil, 0, @no_follow)
+      size = bindings.get(@path.to_s, key, nil, 0, @no_follow)
       raise_error(size) if size == -1
       return unless size > 0
 
       ptr = Slice(LibC::Char).new(size)
-      res = bindings.get(@path, key, ptr, size, @no_follow)
+      res = bindings.get(@path.to_s, key, ptr, size, @no_follow)
       raise_error(res) if res == -1
 
       String.new(ptr)
     end
 
     def []=(key, value)
-      res = bindings.set(@path, key, value, value.bytesize, @no_follow, @only_create, @only_replace)
+      res = bindings.set(@path.to_s, key, value, value.bytesize, @no_follow, @only_create, @only_replace)
       raise_error(res) if res == -1
 
       res
     end
 
     def keys
-      size = bindings.list(@path, nil, 0, @no_follow)
+      size = bindings.list(@path.to_s, nil, 0, @no_follow)
       raise_error(size) if size == -1
       return [] of String unless size > 0
 
       ptr = Slice(LibC::Char).new(size)
-      bindings.list(@path, ptr, size, @no_follow)
+      bindings.list(@path.to_s, ptr, size, @no_follow)
 
       String.new(ptr).split("\000", remove_empty: true).sort
     end
 
     def remove(key)
-      res = bindings.remove(@path, key, @no_follow)
+      res = bindings.remove(@path.to_s, key, @no_follow)
       raise_error(res) if res == -1
 
       res
